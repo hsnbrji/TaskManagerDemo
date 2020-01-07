727fd53d4d60d4152cf861ddfe0375359b3fd938
Hassan Bourji <Hassan.bourji@pin-pay.com>
2020-01-07 14:17:20 +0200

    fix: provided tasks service

--- app/Providers/Tasks/TasksProvider.php
+++ app/Providers/Tasks/TasksProvider.php
@@ -2,12 +2,8 @@
 
 namespace App\Providers\Tasks;
 
-use App\Model\External\Meisertask\Task\MeisertaskTaskCreateParam;
-use App\Model\External\Meisertask\Task\MeisertaskTaskCreateResponse;
-use http\Exception\InvalidArgumentException;
+use App\Services\Tasks\TasksService;
 use Illuminate\Support\ServiceProvider;
-use App\DTO\Tasks\TaskDTO;
-use Symfony\Component\HttpKernel\Exception\BadRequestHttpException;
 
 /**
  * Class TasksProvider
@@ -22,7 +18,9 @@ class TasksProvider extends ServiceProvider {
      */
     public function register()
     {
-        //
+        $this->app->bind('TasksService', function () {
+            return new TasksService();
+        });
     }
 
     /**
@@ -34,34 +32,4 @@ class TasksProvider extends ServiceProvider {
     {
         //
     }
-
-    /**
-     * Service that creates a task in Meistertask and saves it in the database
-     *
-     * @param $taskDto TaskDTO The task to be created
-     * @return TaskDTO
-     */
-    public function createTask(TaskDto $taskDto): TaskDTO {
-        $createdTask = $this->createMeistertaskTask($taskDto);
-        $task = $createdTask->toTask();
-        $task->save();
-    }
-
-    /**
-     * Calls Meisertask Apis to create a task
-     *
-     * @param TaskDTO $taskDTO
-     * @return TaskDTO
-     */
-    private function createMeistertaskTask(TaskDTO $taskDTO): TaskDTO {
-        $client = new \GuzzleHttp\Client();
-        $meisertaskTask = new MeisertaskTaskCreateParam($taskDTO);
-        $url = env("MEISERTASK_API_URL", "") . "1/tasks";
-        $response = $client->post($url, ['body' => json_encode($meisertaskTask)]);
-        if ($response instanceof MeisertaskTaskCreateResponse) {
-            return $response->toTaskDTO();
-        }
-        throw new BadRequestHttpException($response);
-    }
-
 }
